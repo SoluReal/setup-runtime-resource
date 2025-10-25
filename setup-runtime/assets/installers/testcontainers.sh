708cfdb78ac2277131477a6ac0584ca4bfaba6d1
ArjanSchouten <arjan.schouten@solureal.com>
2025-10-25 15:49:50 +0200

    switch to podman for testcontainers.

--- setup-runtime/assets/installers/testcontainers.sh
+++ setup-runtime/assets/installers/testcontainers.sh
@@ -5,7 +5,7 @@ function testcontainers_get_dependencies() {
   testcontainers=$(jq -r '(.source.testcontainers.enabled // "")' <<< "$config")
 
   if [ "$testcontainers" = "true" ]; then
-    echo "jq curl iptables uidmap slirp4netns kmod iproute2 net-tools fuse-overlayfs util-linux"
+    echo "podman"
   else
     echo ""
   fi
@@ -17,33 +17,8 @@ function testcontainers_install() {
   testcontainers=$(jq -r '(.source.testcontainers.enabled // "")' <<< "$config")
 
   if [[ "$testcontainers" = "true" ]]; then
-    info "installing docker..."
-    log_on_error buildah copy "$ctr" "$ROOT_DIR/includes/docker-lib.sh" "$RUNTIME_DIR/docker-lib.sh"
-    log_on_error buildah copy "$ctr" "$ROOT_DIR/includes/docker-cache.sh" "$RUNTIME_DIR/docker-cache.sh"
-    log_on_error buildah run "$ctr" -- bash -lc "
-      set -e
-      curl -sSL https://get.docker.com/ | bash
-      echo 'source $RUNTIME_DIR/docker-lib.sh' >> /root/.bashrc
-      echo 'source $RUNTIME_DIR/docker-cache.sh' >> /root/.bashrc
-    "
-    info "docker installed"
-  fi
-}
-
-function testcontainers_finalize() {
-    local ctr="${1}"
-    local config="${2}"
-    testcontainers=$(jq -r '(.source.testcontainers.enabled // "")' <<< "$config")
-
-  if [[ "$testcontainers" = "true" ]]; then
-    log_on_error buildah run "$ctr" -- bash -lc "
-    echo 'start_docker' >> /root/.bashrc
-    echo 'await_docker' >> /root/.bashrc
-    echo 'trap teardown_docker EXIT' >> /root/.bashrc
-    echo 'docker_load_cache' >> /root/.bashrc
-    echo 'START_TIME=\$(date +%s)' >> /root/.bashrc
-    echo 'unset BASH_ENV' >> /root/.bashrc
-    echo 'unset ENV' >> /root/.bashrc
-    "
+    # See: https://java.testcontainers.org/supported_docker_environment/
+    set_env $ctr "TESTCONTAINERS_RYUK_DISABLED=true"
+    set_env $ctr "DOCKER_HOST=/run/podman/podman.sock"
   fi
 }
