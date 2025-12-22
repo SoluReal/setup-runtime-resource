c889e9ae471d719f0e8c784e7b7fd20bbcd90c5a
ArjanSchouten <arjan.schouten@solureal.com>
2025-12-22 17:07:32 +0100

    install curl when using nvm

--- assets/installers/node.sh
+++ assets/installers/node.sh
@@ -2,7 +2,12 @@
 
 function node_get_dependencies() {
     if [[ -n "$nodejs_version" || "$nvm_enabled" = "true" ]]; then
-      echo "ca-certificates libstdc++6 build-essential"
+      BONUS=""
+      if [[ "$nvm_enabled" = "true" ]]; then
+        # curl is needed for nvm on runtime
+        BONUS="curl"
+      fi
+      echo "ca-certificates libstdc++6 build-essential $BONUS"
     else
       echo ""
     fi
