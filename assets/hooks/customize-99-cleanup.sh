92997d6fd5a768d4465cab37818a8cbc7548eea8
ArjanSchouten <arjan.schouten@solureal.com>
2025-12-29 14:45:35 +0100

    little cleanup

--- assets/hooks/customize-99-cleanup.sh
+++ assets/hooks/customize-99-cleanup.sh
@@ -8,7 +8,7 @@ export SDKMAN_DIR="$chroot_dir$SDKMAN_RUNTIME_DIR"
 
 log_info_hook "Cleaning up"
 if [[ -d $SDKMAN_DIR ]]; then
-    source $SDKMAN_DIR/bin/sdkman-init.sh
+    source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
     set +e
     sdk flush archives
@@ -31,5 +31,18 @@ find $chroot_dir/var -maxdepth 3 -type d -name ".git" ! -path "./.git" -exec rm 
 rm -rf $chroot_dir/var/lib/apt/lists/*
 rm -rf $chroot_dir/var/cache/apt/archives/*
 rm -rf $chroot_dir/tmp/*
+
+# Don't need apt anymore.
+rm -rf $chroot_dir/var/lib/dpkg/*
+rm -rf $chroot_dir/etc/dpkg/*
+rm -rf $chroot_dir/usr/share/dpkg/*
+rm -rf $chroot_dir/usr/libexec/dpkg
+rm -rf $chroot_dir/etc/apt/*
+
+# Don't need perl
+rm -rf $chroot_dir/usr/share/perl*
+rm -rf $chroot_dir/usr/lib/aarch64-linux-gnu/perl*
+rm -rf $chroot_dir/usr/bin/perl*
+rm -rf $chroot_dir/usr/bin/debconf*
 
 log_info_hook "Cleanup finished"
